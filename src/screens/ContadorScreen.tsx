f91c6f35033ecc25252ece5b693d3cc44ca2992d
jenri019 <j.enrique209436@hotmail.com>
2021-06-07 14:40:47 -0500

    Primer commit

---
+++ src/screens/ContadorScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native';
+
+export const ContadorScreen = () => {
+    return (
+        <View>
+            <Text>Contador: XXXX</Text>
+        </View>
+    )
+}
